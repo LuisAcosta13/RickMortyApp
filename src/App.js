c40b6d22077608500bc87d8da6488e62b9fd6560
Luis Acosta <acosta1395@hotmail.com>
2022-08-15 21:33:57 -0300

    agregando funcionalidad

--- src/App.js
+++ src/App.js
@@ -7,15 +7,26 @@ function App() {
 
   const [personajes, setPersonajes] = useState([])
 
-  fetch('https://rickandmortyapi.com/api/character')
-  .then(response => response.json())
-  .then(data => {
-  setPersonajes(data.results)
-  })
+  function showThemAll(){
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => {
+    setPersonajes(data.results)
+    })
+  }
+
+  function hide(id){
+    var elem = document.getElementById(id)
+    elem.style.display = 'none'
+  }
+
 
   return (
-    <div>
+    <div className='render'>
       <Banner/>
+      <div id='button'>
+      <button type='button'  onClick={setTimeout(() => {showThemAll(); hide('button')}, 1000)}>Mostrar todos</button>
+      </div>
       <Cards personajes={personajes}/>
     </div>
   );
