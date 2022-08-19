5e0041b8db47c24023355765ca070c2707d33516
Luis Acosta <acosta1395@hotmail.com>
2022-08-19 18:35:35 -0300

    arreglos de estilo

--- src/componentes/card.jsx
+++ src/componentes/card.jsx
@@ -4,23 +4,23 @@ import './styles/card.css'
 export default function Card({name, image, status, species, location, gender}){
     return(
         <div className="wrap">
-        <div className="card">
-            <div className="front">
-            
-                <img className='pj_img' src={image} alt={name}></img>
-                <h3 className="name">{name}</h3>
+            <div className="card">
+                <div className="front">
                 
-                <p className="status">{status}</p>
+                    <img className='pj_img' src={image} alt={name}></img>
+                    <h3 className="name">{name}</h3>
+                    
+                    <p className="status">{status}</p>
+                </div>
+                <div className="back">
+                    <h3>{name}</h3>
+                    <ul className="list">
+                    <li>Specie: {species}</li>
+                    <li>Gender: {gender}</li>
+                    <li>Last known location: {location.name}</li>
+                    </ul>
+                </div>
             </div>
-            <div className="back">
-                <h3>{name}</h3>
-                <ul className="list">
-                <li>Specie: {species}</li>
-                <li>Gender: {gender}</li>
-                <li>Last known location: {location.name}</li>
-                </ul>
-            </div>
-        </div>
         </div>
     )
     
